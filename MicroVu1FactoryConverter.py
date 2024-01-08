5d1e146cedf98b45443a50124f52ec3e90480676
jeffreytwhitney <jeffrey.t.whitney@gmail.com>
2024-01-08 08:33:20 -0600

    Refactoring

--- MicroVu1FactoryConverter.py
+++ MicroVu1FactoryConverter.py
@@ -1,11 +1,15 @@
 import sys
 from PyQt6 import QtWidgets
 
+from lib import Utilities
 from ui.MicroVuProcessor_MainWindow import MicroVuProcessorMainWindow
 
 
 def main():
+    stylesheet_filepath = Utilities.get_filepath_by_name("MacOS.qss")
+    styleSheet = Utilities.get_file_as_string(stylesheet_filepath)
     app = QtWidgets.QApplication(sys.argv)
+    app.setStyleSheet(styleSheet)
     ui = MicroVuProcessorMainWindow()
     ui.show()
     sys.exit(app.exec())
