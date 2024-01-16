a77dd91e81b8eb61ac27017198e9043b0b0cad3a
jeffreytwhitney <jeffrey.t.whitney@gmail.com>
2024-01-16 13:19:53 -0600

    Adding Processor Tests

--- test/test_CoonRapidsProcessor.py
+++ test/test_CoonRapidsProcessor.py
@@ -54,7 +54,7 @@ def _get_output_root_path() -> str:
     return str(os.path.join(current_dir, "Output"))
 
 
-def get_node_text(line_text: str, search_value: str, start_delimiter: str, end_delimiter: str = "") -> str:
+def _get_node_text(line_text: str, search_value: str, start_delimiter: str, end_delimiter: str = "") -> str:
     if not end_delimiter:
         end_delimiter = start_delimiter
     title_index: int = line_text.upper().find(search_value.upper())
@@ -86,15 +86,15 @@ def _get_unencoded_file_lines(file_path: str) -> list[str]:
         return f.readlines()
 
 
-def get_utf_encoded_file_lines(file_path: str) -> list[str]:
+def _get_utf_encoded_file_lines(file_path: str) -> list[str]:
     if not file_path:
         return []
     with open(file_path, "r", encoding='utf-16-le') as f:
         return f.readlines()
 
 
-def set_node_text(line_text: str, search_value: str, set_value: str, start_delimiter: str,
-                  end_delimiter: str = "") -> str:
+def _set_node_text(line_text: str, search_value: str, set_value: str, start_delimiter: str,
+                   end_delimiter: str = "") -> str:
     current_value: str = MicroVuProgram.get_node_text(line_text, search_value, start_delimiter, end_delimiter)
     current_node: str = search_value + start_delimiter + current_value + end_delimiter
     new_node: str = search_value + start_delimiter + set_value + end_delimiter
@@ -116,7 +116,7 @@ def _store_ini_value(ini_value, ini_section, ini_key):
 
 
 # Setup/Teardown
-def setup_module(micro_vu):
+def setup_module():
     config_filepath = _get_filepath_by_name("TESTSettings.ini")
     os.environ['MICRO_VU_CONVERTER_CONFIG_LOCATION'] = config_filepath
     input_path = _get_input_root_path()
@@ -127,12 +127,13 @@ def setup_module(micro_vu):
     if not os.path.exists(_get_output_directory()):
         os.mkdir(_get_output_directory())
     _processor = lib.MicroVuFileProcessor.get_processor("JTW")
+    micro_vu = MicroVuProgram(_get_input_filepath("446007 END VIEW.iwp"), "10", "A", "")
     _processor.add_micro_vu_program(micro_vu)
+    _processor.process_files()
 
 
 def teardown_module():
     os.environ['MICRO_VU_CONVERTER_CONFIG_LOCATION'] = ""
-    _delete_all_files_in_output_directory()
 
 
 # Fixtures
@@ -143,41 +144,92 @@ def micro_vu() -> MicroVuProgram:
 
 @pytest.fixture(scope="module")
 def micro_vu_lines() -> list[str]:
-    return get_utf_encoded_file_lines(_get_output_filepath("446007 END VIEW.iwp"))
+    output_path = _get_output_filepath("446007 END VIEW.iwp")
+    return _get_utf_encoded_file_lines(output_path)
 
 
 # Tests
-def test_output_file_exists():
-    assert False
+def test_output_file_exists(micro_vu_lines):
+    assert os.path.exists(_get_output_filepath("446007 END VIEW.iwp"))
 
 
 def test_export_filepath(micro_vu_lines):
-    assert False
-
-
-def test_auto_export_filepath(micro_vu_lines):
-    assert False
+    assert _get_node_text(
+            micro_vu_lines[2], "ExpFile", "\"") == "C:\\Users\\Public\\CURL\\in\\446007_OP10_END VIEW_REVA_.csv"
+    assert _get_node_text(
+            micro_vu_lines[2], "AutoExpFile", "\"") == "C:\\Users\\Public\\CURL\\in\\446007_OP10_END VIEW_REVA_.csv"
 
 
 def test_auto_report_filepath(micro_vu_lines):
-    assert False
+    assert _get_node_text(
+            micro_vu_lines[2], "AutoRptFileName", "\"") == "S:\\Micro-Vu\\446007_OP10_END VIEW_REVA_.pdf"
 
 
 def test_instruction_count(micro_vu_lines):
-    assert False
+    assert "Instructions 57" in micro_vu_lines[3]
+
+
+def test_text_kill_exists(micro_vu_lines):
+    assert "TEXT KILL" in micro_vu_lines[4]
 
 
 def test_created_by_exists(micro_vu_lines):
-    assert False
+    assert _get_node_text(
+            micro_vu_lines[10], "Txt", "\"") == "Created By:"
 
 
-def test_comments_exist(micro_vu_lines):
-    assert False
+def test_comments(micro_vu_lines):
+    assert _get_node_text(
+            micro_vu_lines[11], "Txt", "\"") == "Edited By and Comments:"
+    assert "Converted program to work with 1Factory." in micro_vu_lines[11]
 
 
-def test_comments_has_new_comment(micro_vu_lines):
-    assert False
+def test_part_number_is_correct(micro_vu_lines):
+    assert "(Txt \"446007\")" in micro_vu_lines[12]
+    assert "(Name \"PT\")" in micro_vu_lines[12]
 
 
-def test_part_number_text_exists(micro_vu_lines):
-    assert False
+def test_rev_letter_is_correct(micro_vu_lines):
+    assert "(Txt \"A\")" in micro_vu_lines[13]
+    assert "(Name \"REV LETTER\")" in micro_vu_lines[13]
+
+
+def test_op_number_is_correct(micro_vu_lines):
+    assert "(Txt \"10\")" in micro_vu_lines[14]
+    assert "(Name \"OPERATION\")" in micro_vu_lines[14]
+
+
+def test_employee_id_prompt_exists(micro_vu_lines):
+    assert "(Txt \"Enter Employee #\")" in micro_vu_lines[15]
+    assert "(Name \"EMPLOYEE\")" in micro_vu_lines[15]
+
+
+def test_job_number_prompt_exists(micro_vu_lines):
+    assert "(Txt \"Enter Job #\")" in micro_vu_lines[16]
+    assert "(Name \"JOB\")" in micro_vu_lines[16]
+
+
+def test_machine_number_prompt_exists(micro_vu_lines):
+    assert "(Txt \"Enter Machine #\")" in micro_vu_lines[17]
+    assert "(Name \"MACHINE\")" in micro_vu_lines[17]
+
+
+def test_inprocess_text_exists(micro_vu_lines):
+    assert "(Txt \"IN PROCESS\")" in micro_vu_lines[18]
+    assert "(Name \"IN PROCESS\")" in micro_vu_lines[18]
+
+
+def test_sequence_number_prompt_exists(micro_vu_lines):
+    assert "(Txt \"SEQUENCE # IF SETUP PART USE 0 (ZERO).\")" in micro_vu_lines[19]
+    assert "(Name \"SEQUENCE\")" in micro_vu_lines[19]
+
+
+def test_inspection_names(micro_vu_lines):
+    assert "(Name \"INSP_4\")" in micro_vu_lines[177]
+    assert "(Name \"INSP_5\")" in micro_vu_lines[187]
+    assert "(Name \"INSP_28\")" in micro_vu_lines[197]
+    assert "(Name \"INSP_30\")" in micro_vu_lines[203]
+    assert "(Name \"INSP_31A\")" in micro_vu_lines[209]
+    assert "(Name \"INSP_31B\")" in micro_vu_lines[215]
+    assert "(Name \"INSP_32A\")" in micro_vu_lines[221]
+    assert "(Name \"INSP_32B\")" in micro_vu_lines[231]
